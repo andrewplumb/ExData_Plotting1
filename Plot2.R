4fd20cfee5ceb5abaa5ba5124133b3f933c82cc6
andrewplumb <XBBLXKV@L07DBDS015001.ams.bnymellon.net>
2015-08-04 19:17:38 -0400

    Plot 2 update

--- Plot2.R
+++ Plot2.R
@@ -7,6 +7,7 @@ epcfiltered <- filter(epc, Date == "1/2/2007" | Date == "2/2/2007")
 epcfiltered$Date <- dmy(as.character(epcfiltered$Date))
 epcfiltered$Date <- parse_date_time(paste(epcfiltered$Date, epcfiltered$Time), orders = "%Y%m%d %H%M%S")
 
+par(mfrow = c(1,1))
 par(mar = c(4,4,2,2))
 plot(epcfiltered$Date, as.numeric(levels(epcfiltered$Global_active_power))[epcfiltered$Global_active_power], type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
 lines(epcfiltered$Date, as.numeric(levels(epcfiltered$Global_active_power))[epcfiltered$Global_active_power])
